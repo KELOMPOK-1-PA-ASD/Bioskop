016c8c3e280d0fa770756ec259d0405d4c4866d7
Amalia Kartika Sari <121868323+amaliakartikasari@users.noreply.github.com>
2023-04-29 14:56:29 +0800

    Update PA KELOMPOK 1.py

--- PA KELOMPOK 1.py
+++ PA KELOMPOK 1.py
@@ -7,3 +7,19 @@ import os
 from prettytable import PrettyTable
 import mysql.connector
 import pwinput
+
+today = date.today()
+day = today.strftime("%d-%m-%Y")
+now = datetime.now()
+current_time = now.strftime("%H:%M:%S")
+
+tabel = PrettyTable()
+users = PrettyTable()
+
+mydb = mysql.connector.connect(
+  host="db4free.net ",
+  user="kelompok_1",
+  password="kelompoksatu",
+  database="bioskop_1"
+)
+mycursor = mydb.cursor()
